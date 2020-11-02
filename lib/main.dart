24db3eb105dd66ecbe13a25075afd55805ffa206
JAEHO SONG <thruthesky@gmail.com>
2020-11-03 01:09:44 +0900

    register

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:fireflutter_sample_app/screens/home/home.screen.dart';
 import 'package:fireflutter_sample_app/screens/register/register.screen.dart';
 import 'package:flutter/material.dart';
-import 'package:fireflutter/fireflutter.dart';
 import 'package:get/get.dart';
-
-FireFlutter ff = FireFlutter();
+import './global_variables.dart';
 
 void main() async {
   await ff.init();
--- lib/screens/register/register.screen.dart
+++ lib/screens/register/register.screen.dart
@@ -1,5 +1,7 @@
+import 'package:fireflutter_sample_app/global_variables.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
 
 class RegisterScrreen extends StatefulWidget {
   @override
@@ -9,6 +11,8 @@ class RegisterScrreen extends StatefulWidget {
 class _RegisterScrreenState extends State<RegisterScrreen> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  TextEditingController displayNameController = TextEditingController();
+  TextEditingController favoriteColorController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,9 +29,30 @@ class _RegisterScrreenState extends State<RegisterScrreen> {
             controller: passwordController,
             decoration: InputDecoration(hintText: 'Password'),
           ),
+          TextFormField(
+            controller: displayNameController,
+            decoration: InputDecoration(hintText: 'displayName'),
+          ),
+          TextFormField(
+            controller: favoriteColorController,
+            decoration:
+                InputDecoration(hintText: 'What is your favorite color?'),
+          ),
           RaisedButton(
             onPressed: () async {
-              // User
+              try {
+                User user = await ff.register({
+                  'email': emailController.text,
+                  'password': passwordController.text,
+                  'displayName': displayNameController.text,
+                  'favoriteColor': favoriteColorController.text
+                });
+                await Get.defaultDialog(
+                    middleText: 'Welcome ' + user.displayName);
+                Get.toNamed('home');
+              } catch (e) {
+                Get.snackbar('Error', e.toString());
+              }
             },
             child: Text('Register'),
           ),
