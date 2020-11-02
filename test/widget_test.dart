f7a6d81274c7dac1ba64805124f69c2b2fad5b08
JAEHO SONG <thruthesky@gmail.com>
2020-11-02 21:42:26 +0900

    iOS setup

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:fireflutter_sample_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(MainApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
