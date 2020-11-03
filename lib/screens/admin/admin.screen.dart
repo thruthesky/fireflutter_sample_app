2f80c161a07852f1f867ec71b85bc055226658a1
JAEHO SONG <thruthesky@gmail.com>
2020-11-04 01:15:08 +0900

    admin

---
+++ lib/screens/admin/admin.screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AdminScreen extends StatefulWidget {
+  @override
+  _AdminScreenState createState() => _AdminScreenState();
+}
+
+class _AdminScreenState extends State<AdminScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Admin Screen'),
+      ),
+      body: Container(),
+    );
+  }
+}
