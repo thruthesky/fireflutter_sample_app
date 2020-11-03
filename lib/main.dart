5f85a57205f1032dac8ac9632ac96f1d229e6972
JAEHO SONG <thruthesky@gmail.com>
2020-11-03 13:29:05 +0900

    routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:fireflutter_sample_app/screens/home/home.screen.dart';
+import 'package:fireflutter_sample_app/screens/login/login.screen.dart';
+import 'package:fireflutter_sample_app/screens/profile/profile.screen.dart';
 import 'package:fireflutter_sample_app/screens/register/register.screen.dart';
 import 'package:flutter/material.dart';
 import 'package:fireflutter/fireflutter.dart';
@@ -18,7 +20,9 @@ class MainApp extends StatelessWidget {
       initialRoute: 'home',
       getPages: [
         GetPage(name: 'home', page: () => HomeScreen()),
-        GetPage(name: 'home', page: () => RegisterScreen()),
+        GetPage(name: 'register', page: () => RegisterScreen()),
+        GetPage(name: 'login', page: () => LoginScreen()),
+        GetPage(name: 'profile', page: () => ProfileScreen()),
       ],
     );
   }
