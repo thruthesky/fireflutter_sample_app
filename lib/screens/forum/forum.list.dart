2858bbde1f37ef4f8242923dd8bbd1bbb87824ef
JAEHO SONG <thruthesky@gmail.com>
2020-11-04 19:52:21 +0900

    forum list

---
+++ lib/screens/forum/forum.list.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ForumListScreen extends StatefulWidget {
+  @override
+  _ForumListScreenState createState() => _ForumListScreenState();
+}
+
+class _ForumListScreenState extends State<ForumListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
